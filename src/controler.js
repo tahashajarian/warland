906a20873a0993b1b7105d6037e313bd25dc51bb
Taha Shajarian <shajariantaha@gmail.com>
2022-04-16 12:35:29 +0430

    add backward

--- src/controler.js
+++ src/controler.js
@@ -6,111 +6,111 @@ export default class Controler {
       left: false,
       right: false,
       back: false,
-    }
+    };
     this.initDisplay();
-    this.initListener()
-    this.updateDisplay()
+    this.initListener();
+    this.updateDisplay();
   }
 
   initDisplay() {
     // add keys to screen
-    const keysContainer = document.createElement('div');
+    const keysContainer = document.createElement("div");
     Object.entries(this.movement).forEach((key) => {
-      const elemet = document.createElement('span')
-      elemet.innerText = key[0]
-      elemet.id = key[0]
-      elemet.style.position = 'absolute'
-      elemet.style.cursor = 'pointer'
+      const elemet = document.createElement("span");
+      elemet.innerText = key[0];
+      elemet.id = key[0];
+      elemet.style.position = "absolute";
+      elemet.style.cursor = "pointer";
       elemet.ontouchstart = () => {
-        this.movement[key[0]] = true
-      }
+        this.movement[key[0]] = true;
+      };
       elemet.ontouchend = () => {
-        this.movement[key[0]] = false
+        this.movement[key[0]] = false;
+      };
+      if (key[0] === "forward") {
+        elemet.style.top = "-60px";
+        elemet.style.left = "120px";
       }
-      if (key[0] === 'forward') {
-        elemet.style.top = '-60px'
-        elemet.style.left = '120px'
+      if (key[0] === "back") {
+        elemet.style.top = "20px";
+        elemet.style.left = "135px";
       }
-      if (key[0] === 'back') {
-        elemet.style.top = '20px'
-        elemet.style.left = '135px'
+      if (key[0] === "left") {
+        elemet.style.top = "-20px";
+        elemet.style.left = "85px";
       }
-      if (key[0] === 'left') {
-        elemet.style.top = '-20px'
-        elemet.style.left = '85px'
-      }
-      if (key[0] === 'right') {
-        elemet.style.top = '-20px'
-        elemet.style.left = '190px'
-      }
-
-      if (key[0] === 'shift') {
-        elemet.style.top = '-20px'
-        elemet.style.left = '-20px'
+      if (key[0] === "right") {
+        elemet.style.top = "-20px";
+        elemet.style.left = "190px";
       }
 
-      elemet.style.padding = '10px'
-      keysContainer.append(elemet)
-    })
-    keysContainer.style.position = 'fixed'
-    keysContainer.style.bottom = '80px'
-    keysContainer.style.left = '60px'
-    keysContainer.style.color = 'white'
-    keysContainer.style.fontSize = '24px'
-    keysContainer.style.userSelect = 'none'
+      if (key[0] === "shift") {
+        elemet.style.top = "-20px";
+        elemet.style.left = "-20px";
+      }
+
+      elemet.style.padding = "10px";
+      keysContainer.append(elemet);
+    });
+    keysContainer.style.position = "fixed";
+    keysContainer.style.bottom = "80px";
+    keysContainer.style.left = "60px";
+    keysContainer.style.color = "white";
+    keysContainer.style.fontSize = "24px";
+    keysContainer.style.userSelect = "none";
     // keysContainer.style.position = 'fixed'
     // keysContainer.style.position = 'fixed'
 
-    document.body.append(keysContainer)
+    document.body.append(keysContainer);
   }
 
   initListener() {
-    window.addEventListener('keydown', e => {
+    window.addEventListener("keydown", (e) => {
       const key = e.code;
       switch (key) {
-        case 'KeyW':
-          this.movement.forward = true
+        case "KeyW":
+          this.movement.forward = true;
           break;
-        case 'KeyA':
-          this.movement.left = true
+        case "KeyA":
+          this.movement.left = true;
           break;
-        case 'KeyS':
-          this.movement.back = true
+        case "KeyS":
+          this.movement.back = true;
           break;
-        case 'KeyD':
-          this.movement.right = true
+        case "KeyD":
+          this.movement.right = true;
           break;
-        case 'ShiftLeft':
-          this.movement.shift = true
+        case "ShiftLeft":
+          this.movement.shift = true;
           break;
 
         default:
           break;
       }
-    })
-    window.addEventListener('keyup', e => {
+    });
+    window.addEventListener("keyup", (e) => {
       const key = e.code;
       switch (key) {
-        case 'KeyW':
-          this.movement.forward = false
+        case "KeyW":
+          this.movement.forward = false;
           break;
-        case 'KeyA':
-          this.movement.left = false
+        case "KeyA":
+          this.movement.left = false;
           break;
-        case 'KeyS':
-          this.movement.back = false
+        case "KeyS":
+          this.movement.back = false;
           break;
-        case 'KeyD':
-          this.movement.right = false
+        case "KeyD":
+          this.movement.right = false;
           break;
-        case 'ShiftLeft':
-          this.movement.shift = false
+        case "ShiftLeft":
+          this.movement.shift = false;
           break;
 
         default:
           break;
       }
-    })
+    });
   }
   updateDisplay() {
     // console.log(this.movement)
@@ -118,11 +118,11 @@ export default class Controler {
       const el = document.getElementById(key[0]);
       if (el) {
         if (key[1]) {
-          el.style.color = 'red'
+          el.style.color = "red";
         } else {
-          el.style.color = 'white'
+          el.style.color = "white";
         }
       }
-    })
+    });
   }
 }
--- src/objects/character.js
+++ src/objects/character.js
@@ -1,30 +1,29 @@
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
-import * as THREE from 'three'
-
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+import * as THREE from "three";
 
 export class Character {
   constructor(parrent, scene, mixers) {
     this.parrent = parrent;
     this.scene = scene;
     this.mixers = mixers;
-    this.currentAction = 'idle'
-    this.fadeDuration = 0.2
-    this.rotateAngle = new THREE.Vector3(0, 1, 0)
-    this.walkDirection = new THREE.Vector3()
-    this.rotateQuarternion = new THREE.Quaternion()
-    this.cameraTarget = new THREE.Vector3()
+    this.currentAction = "idle";
+    this.fadeDuration = 0.2;
+    this.rotateAngle = new THREE.Vector3(0, 1, 0);
+    this.walkDirection = new THREE.Vector3();
+    this.rotateQuarternion = new THREE.Quaternion();
+    this.cameraTarget = new THREE.Vector3();
 
-    this.runVelocity = 6
-    this.walkVelocity = 3
+    this.runVelocity = 6;
+    this.walkVelocity = 3;
     this.init();
   }
 
   init() {
     this.loadingManager = new THREE.LoadingManager();
     const loader = new FBXLoader(this.loadingManager);
-    this.animations = []
-    loader.setPath('models/');
-    loader.load('man.fbx', (fbx) => {
+    this.animations = [];
+    loader.setPath("models/");
+    loader.load("man.fbx", (fbx) => {
       this.character = fbx;
       fbx.scale.setScalar(0.02);
       const mixer = new THREE.AnimationMixer(fbx);
@@ -45,15 +44,24 @@ export class Character {
 
       this.mixers.push(mixer);
 
+      loader.load("Rifle Walk.fbx", (a) => {
+        onLoad("walk", a);
+      });
+      loader.load("Rifle Run.fbx", (a) => {
+        onLoad("run", a);
+      });
+      loader.load("Rifle Idle.fbx", (a) => {
+        onLoad("idle", a);
+      });
+      loader.load("idle2.fbx", (a) => {
+        onLoad("idle2", a);
+      });
+      loader.load("Backwards Rifle Walk.fbx", (a) => {
+        onLoad("back", a);
+      });
 
-      loader.load('walk.fbx', (a) => { onLoad('walk', a); });
-      loader.load('run.fbx', (a) => { onLoad('run', a); });
-      loader.load('idle.fbx', (a) => { onLoad('idle', a); });
-      loader.load('idle2.fbx', (a) => { onLoad('idle2', a); });
-      loader.load('reverse.fbx', (a) => { onLoad('back', a); });
-
-      this.scene.add(fbx)
-    })
+      this.scene.add(fbx);
+    });
     this.loadingManager.onLoad = () => {
       this.loaded = true;
       this.animations[this.currentAction].action.play();
@@ -65,22 +73,21 @@ export class Character {
     // }
 
     if (this.loaded) {
-
       if (this.parrent.movement.forward) {
         if (this.parrent.movement.shift) {
-          this.playAnimation('run')
+          this.playAnimation("run");
         } else {
-          this.playAnimation('walk')
+          this.playAnimation("walk");
         }
       }
       if (this.parrent.movement.back) {
-        this.playAnimation('back')
+        this.playAnimation("back");
       }
 
       if (!this.parrent.movement.forward && !this.parrent.movement.back) {
-        this.playAnimation('idle')
+        this.playAnimation("idle");
       }
-      this.rotateCharacter(delta)
+      this.rotateCharacter(delta);
     }
   }
 
@@ -88,50 +95,59 @@ export class Character {
     if (animation === this.currentAction) return;
 
     // play animation
-    const toPlay = this.animations[animation].action
-    const current = this.animations[this.currentAction].action
+    const toPlay = this.animations[animation].action;
+    const current = this.animations[this.currentAction].action;
 
-    current.fadeOut(this.fadeDuration)
+    current.fadeOut(this.fadeDuration);
     toPlay.reset().fadeIn(this.fadeDuration).play();
 
-    this.currentAction = animation
+    this.currentAction = animation;
   }
 
   rotateCharacter(delta) {
     // console.log(this.character.position)
     // if (this.currentAction === 'idle') return;
     const angleYCameraDirection = Math.atan2(
-      (this.parrent.camera.position.x - this.character.position.x),
-      (this.parrent.camera.position.z - this.character.position.z))
-    this.rotateQuarternion.setFromAxisAngle(this.rotateAngle, angleYCameraDirection + Math.PI)
-    this.character.quaternion.rotateTowards(this.rotateQuarternion, 0.2)
+      this.parrent.camera.position.x - this.character.position.x,
+      this.parrent.camera.position.z - this.character.position.z
+    );
+    this.rotateQuarternion.setFromAxisAngle(
+      this.rotateAngle,
+      angleYCameraDirection + Math.PI
+    );
+    this.character.quaternion.rotateTowards(this.rotateQuarternion, 0.2);
 
-    if (this.currentAction !== 'idle') {
-
-      this.parrent.camera.getWorldDirection(this.walkDirection)
-      this.walkDirection.y = 0
-      this.walkDirection.normalize()
-      this.walkDirection.applyAxisAngle(this.rotateAngle, Math.PI)
-      const velocity = this.currentAction == 'run' ? this.runVelocity : this.walkVelocity
+    if (this.currentAction !== "idle") {
+      this.parrent.camera.getWorldDirection(this.walkDirection);
+      this.walkDirection.y = 0;
+      this.walkDirection.normalize();
+      this.walkDirection.applyAxisAngle(this.rotateAngle, Math.PI);
+      const velocity =
+        this.currentAction == "run" ? this.runVelocity : this.walkVelocity;
 
       // move model & camera
-      const moveX = this.walkDirection.x * velocity * delta
-      const moveZ = this.walkDirection.z * velocity * delta
-      this.character.position.x -= moveX
-      this.character.position.z -= moveZ
-      this.updateCameraTarget(moveX, moveZ)
+      const moveX = this.walkDirection.x * velocity * delta;
+      const moveZ = this.walkDirection.z * velocity * delta;
+      if (this.parrent.movement.forward) {
+        this.character.position.x -= moveX;
+        this.character.position.z -= moveZ;
+        this.parrent.camera.position.x -= moveX;
+        this.parrent.camera.position.z -= moveZ;
+      } else {
+        this.character.position.x += moveX / 2;
+        this.character.position.z += moveZ / 2;
+        this.parrent.camera.position.x += moveX / 2;
+        this.parrent.camera.position.z += moveZ / 2;
+      }
+      this.updateCameraTarget(moveX, moveZ);
     }
   }
 
   updateCameraTarget(moveX, moveZ) {
-    // move camera
-    this.parrent.camera.position.x -= moveX
-    this.parrent.camera.position.z -= moveZ
-
     // update camera target
-    this.cameraTarget.x = this.character.position.x
-    this.cameraTarget.y = this.character.position.y 
-    this.cameraTarget.z = this.character.position.z
-    this.parrent.controls.target = this.cameraTarget
+    this.cameraTarget.x = this.character.position.x;
+    this.cameraTarget.y = this.character.position.y;
+    this.cameraTarget.z = this.character.position.z;
+    this.parrent.controls.target = this.cameraTarget;
   }
 }
